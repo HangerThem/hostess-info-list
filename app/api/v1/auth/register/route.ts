37209649511869806b7b49618ab42349bed2fb62
HangerThem <f.borisjuk@gmail.com>
2023-09-30 17:59:15 +0200

    Temp registration disable

--- app/api/v1/auth/register/route.ts
+++ app/api/v1/auth/register/route.ts
@@ -1,26 +1,36 @@
-import { PrismaClient } from "@prisma/client"
-import { hashPassword } from "@/utils/hashPassword"
+// import { PrismaClient } from "@prisma/client"
+// import { hashPassword } from "@/utils/hashPassword"
 
 export async function POST(req: Request) {
-  const { username, password } = await req.json()
-  const prisma = new PrismaClient()
-
-  const user = await prisma.user.create({
-    data: {
-      username,
-      password: await hashPassword(password),
-    },
-  })
-
   return new Response(
     JSON.stringify({
-      message: "Registration successful",
-      success: true,
-      user,
+      message: "Registration failed",
+      success: false,
     }),
     {
-      status: 200,
+      status: 503,
       headers: { "content-type": "application/json" },
     }
   )
+  // const { username, password } = await req.json()
+  // const prisma = new PrismaClient()
+
+  // const user = await prisma.user.create({
+  //   data: {
+  //     username,
+  //     password: await hashPassword(password),
+  //   },
+  // })
+
+  // return new Response(
+  //   JSON.stringify({
+  //     message: "Registration successful",
+  //     success: true,
+  //     user,
+  //   }),
+  //   {
+  //     status: 200,
+  //     headers: { "content-type": "application/json" },
+  //   }
+  // )
 }
