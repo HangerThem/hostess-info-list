b068a2a1081080e88331049d89f9da7d84a6a30d
František Borisjuk <f.borisjuk@gmail.com>
2023-10-08 13:55:14 +0200

    Update route.ts

--- app/api/v1/auth/login/route.ts
+++ app/api/v1/auth/login/route.ts
@@ -2,9 +2,10 @@ import { PrismaClient } from "@prisma/client"
 import { generateToken } from "@/lib/token"
 import { compare } from "bcryptjs"
 
+const prisma = new PrismaClient()
+
 export async function POST(req: Request) {
   const { username, password } = await req.json()
-  const prisma = new PrismaClient()
   const user = await prisma.user.findUnique({ where: { username } })
   if (!user) {
     return new Response(JSON.stringify({ message: "Login failed" }), {
