e1eb180274d640f15ba69989f723907992f45bdb
HangerThem <f.borisjuk@gmail.com>
2023-10-09 13:50:40 +0200

    Temp register

--- app/api/v1/auth/register/route.ts
+++ app/api/v1/auth/register/route.ts
@@ -1,37 +1,42 @@
-// import { PrismaClient } from "@prisma/client"
-// import { hashPassword } from "@/utils/hashPassword"
+import { PrismaClient } from "@prisma/client"
+import { hashPassword } from "@/utils/hashPassword"
+import { generateToken } from "@/lib/token"
 
-// const prisma = new PrismaClient()
+const prisma = new PrismaClient()
 
 export async function POST(req: Request) {
-  return new Response(
-    JSON.stringify({
-      message: "Registration failed",
-      success: false,
-    }),
-    {
-      status: 503,
-      headers: { "content-type": "application/json" },
-    }
-  )
-  // const { username, password } = await req.json()
-
-  // const user = await prisma.user.create({
-  //   data: {
-  //     username,
-  //     password: await hashPassword(password),
-  //   },
-  // })
-
   // return new Response(
   //   JSON.stringify({
-  //     message: "Registration successful",
-  //     success: true,
-  //     user,
+  //     message: "Registration failed",
+  //     success: false,
   //   }),
   //   {
-  //     status: 200,
+  //     status: 503,
   //     headers: { "content-type": "application/json" },
   //   }
   // )
+  const { username, password } = await req.json()
+
+  const user = await prisma.user.create({
+    data: {
+      username,
+      password: await hashPassword(password),
+    },
+  })
+
+  const token = generateToken({
+    id: user.id,
+  })
+
+  return new Response(
+    JSON.stringify({
+      message: "Registration successful",
+      success: true,
+      token,
+    }),
+    {
+      status: 200,
+      headers: { "content-type": "application/json" },
+    }
+  )
 }
