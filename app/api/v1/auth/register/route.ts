acacf1cfbe5738d99500e4fb06da5b21fb9e057e
HangerThem <f.borisjuk@gmail.com>
2023-10-09 13:42:59 +0200

    Temp register removal

--- app/api/v1/auth/register/route.ts
+++ app/api/v1/auth/register/route.ts
@@ -1,37 +1,37 @@
-import { PrismaClient } from "@prisma/client"
-import { hashPassword } from "@/utils/hashPassword"
+// import { PrismaClient } from "@prisma/client"
+// import { hashPassword } from "@/utils/hashPassword"
 
-const prisma = new PrismaClient()
+// const prisma = new PrismaClient()
 
 export async function POST(req: Request) {
-  // return new Response(
-  //   JSON.stringify({
-  //     message: "Registration failed",
-  //     success: false,
-  //   }),
-  //   {
-  //     status: 503,
-  //     headers: { "content-type": "application/json" },
-  //   }
-  // )
-  const { username, password } = await req.json()
-
-  const user = await prisma.user.create({
-    data: {
-      username,
-      password: await hashPassword(password),
-    },
-  })
-
   return new Response(
     JSON.stringify({
-      message: "Registration successful",
-      success: true,
-      user,
+      message: "Registration failed",
+      success: false,
     }),
     {
-      status: 200,
+      status: 503,
       headers: { "content-type": "application/json" },
     }
   )
+  // const { username, password } = await req.json()
+
+  // const user = await prisma.user.create({
+  //   data: {
+  //     username,
+  //     password: await hashPassword(password),
+  //   },
+  // })
+
+  // return new Response(
+  //   JSON.stringify({
+  //     message: "Registration successful",
+  //     success: true,
+  //     user,
+  //   }),
+  //   {
+  //     status: 200,
+  //     headers: { "content-type": "application/json" },
+  //   }
+  // )
 }
